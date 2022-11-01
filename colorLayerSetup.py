a29650166dda0753fc92bc4fd9a22522174426c0
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2022-11-01 18:26:02 +0000

    Update colorLayerSetup.py

--- colorLayerSetup.py
+++ colorLayerSetup.py
@@ -1,3 +1,12 @@
+import nuke
+
+COLOR_LAYER_PREFIX = 'C_'
+EXPR_ALPHA = 'channel0 alpha expr0 clamp(r+g+b)'
+X_DIST = 1000
+Y_DIST = 200
+GRADE_DIST = 2500
+SELECT_VAL = True
+
 def colorLayerSetup():
     nodes = nuke.selectedNodes()
     if not len(nodes) == 1:
@@ -15,59 +24,54 @@ def colorLayerSetup():
             channelLayers = list(set(layersList))
             colorGroup = []
             for c in channelLayers:
-                if c.startswith("C_"):
+                if c.startswith(COLOR_LAYER_PREFIX):
                     colorGroup.append(c)
                 else:
                     continue
             LayersNames = '' + '\n'.join(colorGroup)
             print(channelLayers)
-        
+
             if not colorGroup:
                 nuke.message('<font color=orange><h3>No color passes found!\nAvailable layers are:</h3>\n' + str(channelLayers))
             else:
-                xDist = 1000
-                yDist = 200
-                gradeDist = 2500
-                selectVal = True
-            
                 # create dotMain
                 dotMain = nuke.nodes.Dot()
                 dotMain['xpos'].setValue(int(curSel['xpos'].value()) + (curSel.screenWidth() / 2) - (dotMain.screenWidth() / 2))
                 dotMain['ypos'].setValue(int(curSel['ypos'].value()) + 300)
-                dotMain.setSelected(selectVal)
+                dotMain.setSelected(SELECT_VAL)
                 dotMain.setInput(0, curSel)
-            
+
                 # create dot
                 dot = connectDot = nuke.nodes.Dot()
-                dot['xpos'].setValue(int(dotMain['xpos'].value()) - xDist)
+                dot['xpos'].setValue(int(dotMain['xpos'].value()) - X_DIST)
                 dot['ypos'].setValue(int(dotMain['ypos'].value()))
-                dot.setSelected(selectVal)
+                dot.setSelected(SELECT_VAL)
                 dot.setInput(0, dotMain)
-            
+
                 # create removeN
                 removeN = nuke.nodes.Remove()
                 removeN['operation'].setValue('remove')
                 removeN['channels'].setValue('all')
                 removeN['xpos'].setValue(int(connectDot['xpos'].value()) - 36)
                 removeN['ypos'].setValue(int(connectDot['ypos'].value()) + 200)
-                removeN.setSelected(selectVal)
+                removeN.setSelected(SELECT_VAL)
                 removeN.setInput(0, connectDot)
-            
+
                 mergeFromold = None
                 mergePlusOld = None
-            
+
                 for index, n in enumerate(colorGroup):
                     # creating dot
                     newX = dot['xpos'].value()
                     newY = dot['ypos'].value()
                     oldDot = dot
                     dot = nuke.nodes.Dot()
-                    dot['xpos'].setValue(int(newX) - xDist)
+                    dot['xpos'].setValue(int(newX) - X_DIST)
                     dot['ypos'].setValue(newY)
-                    dot.setSelected(selectVal)
+                    dot.setSelected(SELECT_VAL)
                     newDot = dot
                     newDot.setInput(0, oldDot)
-            
+
                     # creating shuffle
                     shuffle = nuke.nodes.Shuffle2()
                     shuffle['in1'].setValue(n)
@@ -75,98 +79,98 @@ def colorLayerSetup():
                     shuffle['note_font_size'].setValue(25)
                     shuffle['xpos'].setValue(int(dot['xpos'].value()) - (shuffle.screenWidth() / 2) + (dot.screenWidth() / 2))
                     shuffle['ypos'].setValue(int(dot['ypos'].value()) + 100)
-                    shuffle.setSelected(selectVal)
+                    shuffle.setSelected(SELECT_VAL)
                     shuffle.setInput(0, newDot)
-            
+
                     # creating dotshuf
                     dotshuf = nuke.nodes.Dot()
                     dotshuf['xpos'].setValue(
                         int(shuffle['xpos'].value()) - (dotshuf.screenWidth() / 2) + (shuffle.screenWidth() / 2))
-                    dotshuf['ypos'].setValue(int(dot['ypos'].value()) + int(index * yDist) + 250)
-                    dotshuf.setSelected(selectVal)
+                    dotshuf['ypos'].setValue(int(dot['ypos'].value()) + int(index * Y_DIST) + 250)
+                    dotshuf.setSelected(SELECT_VAL)
                     dotshuf.setInput(0, shuffle)
-            
+
                     # creating mergeFrom
                     mergeFrom = nuke.nodes.Merge2()
-                    mergeFrom['xpos'].setValue(int(curSel['xpos'].value()) - (len(colorGroup) + 2) * xDist)
+                    mergeFrom['xpos'].setValue(int(curSel['xpos'].value()) - (len(colorGroup) + 2) * X_DIST)
                     mergeFrom['ypos'].setValue(int(dotshuf['ypos'].value()) - (mergeFrom.screenHeight() / 2))
                     mergeFrom['operation'].setValue('from')
-                    mergeFrom.setSelected(selectVal)
+                    mergeFrom.setSelected(SELECT_VAL)
                     mergeFrom.setInput(1, dotshuf)
                     if mergeFrom:
                         mergeFrom.setInput(0, mergeFromold)
                     if index == 0:
                         mergefirst = mergeFrom
-            
+
                     if index + 1 == len(colorGroup):
                         # creating dotUpCorner
                         dotUpCorner = nuke.nodes.Dot()
                         dotUpCorner['xpos'].setValue(int(mergeFrom['xpos'].value()) + 36)
                         dotUpCorner['ypos'].setValue(int(newDot['ypos'].value()))
-                        dotUpCorner.setSelected(selectVal)
+                        dotUpCorner.setSelected(SELECT_VAL)
                         dotUpCorner.setInput(0, newDot)
-            
+
                         mergefirst.setInput(0, dotUpCorner)
-            
+
                     mergeFromold = mergeFrom
-            
+
                     # creating dotgrade
                     dotgrade = nuke.nodes.Dot()
                     dotgrade['xpos'].setValue(int(dotshuf['xpos'].value()))
-                    dotgrade['ypos'].setValue(int(dot['ypos'].value()) + int((len(colorGroup) + 2) * yDist) + 250)
-                    dotgrade.setSelected(selectVal)
+                    dotgrade['ypos'].setValue(int(dot['ypos'].value()) + int((len(colorGroup) + 2) * Y_DIST) + 250)
+                    dotgrade.setSelected(SELECT_VAL)
                     dotgrade.setInput(0, dotshuf)
-            
+
                     # creating unprem
                     unprem = nuke.nodes.Unpremult()
                     unprem['xpos'].setValue(int(shuffle['xpos'].value()))
                     unprem['ypos'].setValue(int(dotgrade['ypos'].value()) + 100)
-                    unprem.setSelected(selectVal)
+                    unprem.setSelected(SELECT_VAL)
                     unprem.setInput(0, dotgrade)
-            
+
                     # creating prem
                     prem = nuke.nodes.Premult()
                     prem['xpos'].setValue(int(shuffle['xpos'].value()))
-                    prem['ypos'].setValue(int(dotgrade['ypos'].value()) + gradeDist - 100)
-                    prem.setSelected(selectVal)
+                    prem['ypos'].setValue(int(dotgrade['ypos'].value()) + GRADE_DIST - 100)
+                    prem.setSelected(SELECT_VAL)
                     prem.setInput(0, unprem)
-            
+
                     # creating dotgrade2
                     dotgrade2 = nuke.nodes.Dot()
                     dotgrade2['xpos'].setValue(int(dotgrade['xpos'].value()))
-                    dotgrade2['ypos'].setValue(int(dotgrade['ypos'].value()) + gradeDist)
-                    dotgrade2.setSelected(selectVal)
+                    dotgrade2['ypos'].setValue(int(dotgrade['ypos'].value()) + GRADE_DIST)
+                    dotgrade2.setSelected(SELECT_VAL)
                     dotgrade2.setInput(0, prem)
-            
+
                     # creating expAlpha
-                    expAlpha = nuke.nodes.Expression()
+                    expAlpha = nuke.createNode('Expression', EXPR_ALPHA, inpanel=False)
                     expAlpha['channel0'].setValue('alpha')
-                    expAlpha['expr0'].setValue('clamp(r+g+b)')
+                    #expAlpha['expr0'].setValue('clamp(r+g+b)')
                     expAlpha['xpos'].setValue(int(shuffle['xpos'].value()))
                     expAlpha['ypos'].setValue(int(dotgrade2['ypos'].value()) + 200)
-                    expAlpha.setSelected(selectVal)
+                    expAlpha.setSelected(SELECT_VAL)
                     expAlpha.setInput(0, dotgrade2)
-            
+
                     # creating dotExpr
                     dotExpr = nuke.nodes.Dot()
                     dotExpr['xpos'].setValue(int(dotgrade['xpos'].value()))
-                    dotExpr['ypos'].setValue(int(expAlpha['ypos'].value()) + int(index * yDist) + 250)
-                    dotExpr.setSelected(selectVal)
+                    dotExpr['ypos'].setValue(int(expAlpha['ypos'].value()) + int(index * Y_DIST) + 250)
+                    dotExpr.setSelected(SELECT_VAL)
                     dotExpr.setInput(0, expAlpha)
-            
+
                     # creating mergePlus
                     mergePlus = nuke.nodes.Merge2()
                     mergePlus['xpos'].setValue(int(removeN['xpos'].value()))
                     mergePlus['ypos'].setValue(int(dotExpr['ypos'].value()))
                     mergePlus['operation'].setValue('plus')
-                    mergePlus.setSelected(selectVal)
+                    mergePlus.setSelected(SELECT_VAL)
                     mergePlus.setInput(1, dotExpr)
                     if mergePlusOld:
                         mergePlus.setInput(0, mergePlusOld)
                     else:
                         mergePlus.setInput(0, removeN)
                     mergePlusOld = mergePlus
-            
+
                 # creating shuffleExtra
                 shuffle = nuke.nodes.Shuffle2()
                 shuffle['in1'].setValue('rgba')
@@ -175,56 +179,56 @@ def colorLayerSetup():
                 shuffle['note_font_size'].setValue(25)
                 shuffle['xpos'].setValue(int(mergeFrom['xpos'].value()))
                 shuffle['ypos'].setValue(int(mergeFrom['ypos'].value()) + 100)
-                shuffle.setSelected(selectVal)
+                shuffle.setSelected(SELECT_VAL)
                 shuffle.setInput(0, mergeFrom)
-            
+
                 # creating expAlphaSide
-                expAlphaSide = nuke.nodes.Expression()
+                expAlphaSide = nuke.createNode('Expression', EXPR_ALPHA, inpanel=False)
                 expAlphaSide['channel0'].setValue('alpha')
-                expAlphaSide['expr0'].setValue('clamp(r+g+b)')
+                #expAlphaSide['expr0'].setValue('clamp(r+g+b)')
                 expAlphaSide['xpos'].setValue(int(shuffle['xpos'].value()))
                 expAlphaSide['ypos'].setValue(int(expAlpha['ypos'].value()))
-                expAlphaSide.setSelected(selectVal)
+                expAlphaSide.setSelected(SELECT_VAL)
                 expAlphaSide.setInput(0, shuffle)
-            
+
                 # creating dotExprSide
                 dotExprSide = nuke.nodes.Dot()
                 dotExprSide['xpos'].setValue(int(expAlphaSide['xpos'].value()) + 36)
-                dotExprSide['ypos'].setValue(int(dotExpr['ypos'].value()) + int(yDist))
-                dotExprSide.setSelected(selectVal)
+                dotExprSide['ypos'].setValue(int(dotExpr['ypos'].value()) + int(Y_DIST))
+                dotExprSide.setSelected(SELECT_VAL)
                 dotExprSide.setInput(0, expAlphaSide)
-            
+
                 # creating mergePlusSide
                 mergePlusSide = nuke.nodes.Merge2()
                 mergePlusSide['xpos'].setValue(int(mergePlus['xpos'].value()))
                 mergePlusSide['ypos'].setValue(int(dotExprSide['ypos'].value()))
                 mergePlusSide['operation'].setValue('plus')
-                mergePlusSide.setSelected(selectVal)
+                mergePlusSide.setSelected(SELECT_VAL)
                 mergePlusSide.setInput(1, dotExprSide)
                 mergePlusSide.setInput(0, mergePlusOld)
-            
+
                 # creating dotMsg
                 ## TODO - add NAN_INF_KILLER _ nuke.createNode('NST_NAN_INF_Killer') - causing connestion problems
                 dotMsg = nuke.nodes.Dot()
                 dotMsg['xpos'].setValue(int(connectDot['xpos'].value()))
-                dotMsg['ypos'].setValue(int(mergePlusSide['ypos'].value()) + (yDist * 4))
+                dotMsg['ypos'].setValue(int(mergePlusSide['ypos'].value()) + (Y_DIST * 4))
                 dotMsg['label'].setValue(' Add a <b>NAN_INF_Killer</b> node here!')
                 dotMsg['note_font_size'].setValue(32)
-                dotMsg.setSelected(selectVal)
+                dotMsg.setSelected(SELECT_VAL)
                 dotMsg.setInput(0, mergePlusSide)
-            
+
                 # creating dotCorner
                 dotCorner = nuke.nodes.Dot()
                 dotCorner['xpos'].setValue(int(connectDot['xpos'].value()))
-                dotCorner['ypos'].setValue(int(dotMsg['ypos'].value()) + yDist)
-                dotCorner.setSelected(selectVal)
+                dotCorner['ypos'].setValue(int(dotMsg['ypos'].value()) + Y_DIST)
+                dotCorner.setSelected(SELECT_VAL)
                 dotCorner.setInput(0, dotMsg)
-            
+
                 # creating copyNode
                 copyNode = nuke.nodes.Copy()
                 copyNode['xpos'].setValue(int(curSel['xpos'].value()))
                 copyNode['ypos'].setValue(int(dotCorner['ypos'].value()))
                 copyNode['channels'].setValue('rgb')
-                copyNode.setSelected(selectVal)
+                copyNode.setSelected(SELECT_VAL)
                 copyNode.setInput(1, dotCorner)
                 copyNode.setInput(0, dotMain)
