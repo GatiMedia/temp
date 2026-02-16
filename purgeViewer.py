e15e75a74209ee6e865c4075d0cea94e0de06262
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2026-02-16 11:12:10 +0000

    Update purgeViewer.py

--- purgeViewer.py
+++ purgeViewer.py
@@ -1,37 +1,19 @@
-def findAndDeleteViewerNodes():
-    viewers=[]
-    for n in nuke.allNodes('Viewer'):
-        viewers.append(n.fullName())
-        nuke.delete(n)
-    if viewers:
-        viewers=str((', '.join(viewers)))
+def delete_all_viewers():
+    deleted_nodes = []
+
+    def find_and_delete(container):
+        for n in container.nodes():
+            if n.Class() == "Viewer":
+                deleted_nodes.append(n.fullName())
+                nuke.delete(n)
+            elif isinstance(n, nuke.Group):
+                find_and_delete(n)
+
+    find_and_delete(nuke.root())
+
+    if deleted_nodes:
+        deleted_nodes_num=(f"Total nodes removed: {len(deleted_nodes)}")
+        deleted_nodes=str('\n'.join(deleted_nodes))
+        nuke.message(f"<font color=orange><center><h3>Deleted the following Viewer nodes:\n\n<font color=aqua>{deleted_nodes}\n\n<font color=orange>{deleted_nodes_num}")
     else:
-        viewers=None
-    return viewers
-
-
-def purgeGroups():
-    all_viewers=[]
-
-
-
-    all_viewers = [x for x in all_viewers if x is not None]
-    print(all_viewers)
-purgeGroups()
-
-
-
-def purgeGroups():
-    all_viewers=[]
-    for g in nuke.allNodes('Group'):
-        with g:
-            all_viewers.append(findAndDeleteViewerNodes())
-            for g in nuke.allNodes('Group'):
-                with g:
-                    all_viewers.append(findAndDeleteViewerNodes())
-                    for g in nuke.allNodes('Group'):
-                        with g:
-                            all_viewers.append(findAndDeleteViewerNodes())
-    all_viewers = [x for x in all_viewers if x is not None]
-    print(all_viewers)
-purgeGroups()
+        nuke.message("<font color=aqua><center><h3>No Viewer nodes found.")
